b7283c5353a628b63bc8acef37735507b8f2b306
rakesh-gupta29 <rakesh.rkgupta29@gmail.com>
2026-03-15 09:31:02 +0530

    Added handlers and parsing

--- main.go
+++ main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
+	"encoding/csv"
 	"encoding/json"
 	"fmt"
 	"net/http"
 )
-
-// --- Error helpers ---
 
 type APIError struct {
 	Code    int    `json:"code"`
@@ -22,6 +21,10 @@ func writeJSONError(w http.ResponseWriter, code int, message string) {
 type EmailRecord struct {
 	Email string
 	Name  string
+}
+type VerifyRequest struct {
+	id      string
+	records []EmailRecord
 }
 
 func route(mux *http.ServeMux, method string, pattern string, h http.HandlerFunc) {
@@ -39,7 +42,50 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func verifyHandler(w http.ResponseWriter, r *http.Request) {
-	// TODO: parse CSV, run email validation
+	if err := r.ParseMultipartForm(10 << 20); err != nil {
+		writeJSONError(w, http.StatusBadRequest, "File is too big")
+		return
+	}
+
+	file, _, err := r.FormFile("file")
+	if err != nil {
+		writeJSONError(w, http.StatusBadRequest, "file not found")
+		return
+	}
+
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+
+	// read and discard the header
+	if _, err := reader.Read(); err != nil {
+		writeJSONError(w, http.StatusBadRequest, "failed to read header")
+		return
+	}
+
+	records, err := reader.ReadAll()
+	if err != nil {
+		writeJSONError(w, http.StatusBadRequest, "failed to pares the file")
+		return
+	}
+
+	var req VerifyRequest
+
+	for _, row := range records {
+		if len(row) < 2 {
+			writeJSONError(w, http.StatusBadRequest, "bad row found")
+			return
+		}
+		req.records = append(req.records, EmailRecord{
+			Email: row[0],
+			Name:  row[1],
+		})
+
+		records = records[1:]
+	}
+
+	fmt.Println(req)
+
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(map[string]string{"status": "ok"})
 }
